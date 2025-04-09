d9cbe9f037c08297d2553ffebee4295f77dcff43
Vardaan Dwivedi <163497051+VARDAAN08@users.noreply.github.com>
2025-04-09 17:13:02 +0530

    Create AuthPage.jsx

---
+++ frontend/src/pages/src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+
+ import Layout from '../components/Layout';
+ import AuthForms from '../components/AuthForms';
+ 
+ const AuthPage = () => {
+   return (
+     <Layout>
+       <AuthForms />
+     </Layout>
+   );
+ };
+ 
+ export default AuthPage;
